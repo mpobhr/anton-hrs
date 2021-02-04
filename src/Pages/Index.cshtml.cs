9d20b59696168db5c20b26b762f7130d1438e083
Anton Heryanto Hasan <anton.heryanto@gmail.com>
2021-02-04 14:12:48 +0800

    staff listing

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -10,15 +10,16 @@ namespace Hrs.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<string> Items { get; set; } 
+        public List<Staff> Items { get; set; } 
+                
         public void OnGet()
-        {
-            Items = new List<string> {
-                "Anton Heryanto Hasan",
-                "Aishah Rodzi",
-                "Faiq ITCS",
-                "Liyana",
-                "Muhamad Zul Razak"
+        {            
+            Items = new List<Staff> {
+                new Staff { StaffId="K01231", Name = "Anton Heryanto Hasan" },
+                new Staff { StaffId="K01232", Name = "Aishah Rodzi" },
+                new Staff { StaffId="K01233", Name = "Faiq ITCS" },
+                new Staff { StaffId="K01234", Name = "Liyana" },
+                new Staff { StaffId="K01235", Name = "Muhamad Zul Razak" },
             };
         }
     }
